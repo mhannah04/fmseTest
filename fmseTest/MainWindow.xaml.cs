52751a64b76204106620c05e16aebe7a214d149f
Max <max.hannah4@outlook.com>
2025-04-10 18:23:26 +0100

    Updated GUI

--- fmseTest/MainWindow.xaml.cs
+++ fmseTest/MainWindow.xaml.cs
@@ -15,14 +15,79 @@ using System.Windows.Shapes;
 
 namespace fmseTest
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private List<Button> myButtons;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            myButtons = new List<Button>
+            {
+                AwardsButton,
+                NationsButton,
+                CitiesButton,
+                ClubsButton,
+                CompetitionsButton,
+                PlayersButton,
+                StaffButton
+
+            };
         }
+
+        private void FileButton_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Button pressed successfully");
+        }
+
+        private void AwardsButton_Click(object sender, RoutedEventArgs e)
+        {
+            buttonInit(AwardsButton);
+        }
+
+        private void NationsButton_Click(object sender, RoutedEventArgs e)
+        {
+            buttonInit(NationsButton);
+        }
+
+        private void CitiesButton_Click(object sender, RoutedEventArgs e)
+        {
+            buttonInit(CitiesButton);
+        }
+
+        private void ClubsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            buttonInit(ClubsButton);
+        }
+
+        private void CompetitionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            buttonInit(CompetitionsButton);
+        }
+
+        private void PlayersButton_Click(object sender, RoutedEventArgs e)
+        {
+            buttonInit(PlayersButton);
+        }
+
+        private void StaffButton_Click(object sender, RoutedEventArgs e)
+        {
+            buttonInit(StaffButton);
+        }
+
+
+        private void buttonInit(Button clickedButton)
+        {
+            foreach (Button button in myButtons)
+            {
+                button.BorderThickness = new Thickness(0,0, 0, 0);
+            }
+
+            clickedButton.BorderThickness = new Thickness(1, 1, 1, 0);
+        }
+
     }
 }
